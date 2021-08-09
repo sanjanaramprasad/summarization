2e6cf47a1f09f1209cbecced23c667c9da0e155b
sanjanaramprasad <sanjanaramprasad1@gmail.com>
2021-08-09 15:46:47 -0400

    encoders self attn

--- scripts/bart_hat/check_modelHAT.py
+++ scripts/bart_hat/check_modelHAT.py
@@ -60,7 +60,59 @@ tokenizer.add_tokens(additional_special_tokens)
 
 class BartMultiEncHATTester():
 
+    def test_model_forward_bart_encoder_flat(self, encoder_combination_type):
+        from BartMultiEncFlattened_HAT import BartMultiEncFlatHAT
+        
+        model = BartMultiEncFlatHAT.from_pretrained('facebook/bart-base')
+        model._make_duplicate_encoders(layer_share = False)
+        model.resize_token_embeddings(len(tokenizer))
+        print("Loading Data ...")
+        summary_data = make_data(tokenizer, SummaryDataModule, path = '/home/sanjana', files = ['train_rr_data.csv', 
+                            'dev_rr_data.csv', 'test_rr_data.csv'])
+        summary_data.setup("stage")
+        val_data = summary_data.val_dataloader(data_type = 'robo')
+        print("Done.")
+        it = iter(val_data)
 
+        data = next(it)
+        population_input_ids, population_attention_masks, population_bos_ids,\
+                interventions_input_ids, interventions_attention_masks, interventions_bos_ids,\
+                outcomes_input_ids, outcomes_attention_masks, outcomes_bos_ids,\
+                punchline_text_input_ids, punchline_text_attention_masks, punchline_text_bos_ids,\
+                punchline_effect_input_ids, punchline_effect_attention_masks, punchline_effect_bos_ids = get_data(data)
+
+        print("forward...") 
+        tgt_ids = data[-1]
+        outputs = model(
+            input_ids_col0 = population_input_ids,
+            input_ids_col1 = interventions_input_ids,
+            input_ids_col2 = outcomes_input_ids, 
+            input_ids_col3 = punchline_text_input_ids,
+            input_ids_col4 = punchline_effect_input_ids,
+            attention_mask_col0 = population_attention_masks,
+            attention_mask_col1 = interventions_attention_masks,
+            attention_mask_col2 = outcomes_attention_masks,
+            attention_mask_col3 = punchline_text_attention_masks,
+            attention_mask_col4 = punchline_effect_attention_masks,
+            bos_ids_col0 = population_bos_ids,
+            bos_ids_col1 = interventions_bos_ids,
+            bos_ids_col2 = outcomes_bos_ids,
+            bos_ids_col3 = punchline_text_bos_ids,
+            bos_ids_col4 = punchline_effect_bos_ids,
+            labels = tgt_ids,
+            encoder_combination_type = encoder_combination_type,
+            use_cache = False,
+        )
+
+        #tgt_ids = data[-1]
+        optimizer = optim.Adam(model.parameters())
+        loss = outputs[0]
+        #ce_loss_fct = torch.nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)
+        #loss = ce_loss_fct(lm_logits.view(-1, lm_logits.shape[-1]), tgt_ids.view(-1))
+        optimizer.zero_grad()
+        loss.backward()
+        print("OUTPUTS", outputs[0])
+        print('=' *13)
 
     def test_model_forward_bart_encoder(self, encoder_combination_type):
         from BartMultiEncHAT import BartMultiEncHAT
@@ -119,5 +171,6 @@ class BartMultiEncHATTester():
         
 
 obj = BartMultiEncHATTester()
-obj.test_model_forward_bart_encoder(encoder_combination_type = 'HAT')
+#obj.test_model_forward_bart_encoder(encoder_combination_type = 'HAT')
+obj.test_model_forward_bart_encoder_flat(encoder_combination_type='self_attention')
 #obj.test_model_forward_bart_encoder_loop_per_study(encoder_combination_type = 'linearize')
